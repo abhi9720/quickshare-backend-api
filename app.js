d3124e6b04fff185c347ac366b471497c3ef3b77
abhi9720 <abhishektiwari9720@gmail.com>
2021-05-20 18:17:53 +0530

    update app routers

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.use((error, req, res, next) => {
 	});
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
 	console.log(`Serving on port ${port}`);
 });
